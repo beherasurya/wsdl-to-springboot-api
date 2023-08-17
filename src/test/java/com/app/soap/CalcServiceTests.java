362645be68874da06284c87816f294b3f55b459d
Surya <115415921+beherasurya@users.noreply.github.com>
2023-08-17 10:17:13 +0530

    test file uploaded

---
+++ src/test/java/com/app/soap/CalcServiceTests.java
@@ -0,0 +1,36 @@
+package com.app.soap.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CalcServiceTests {
+
+	@Autowired
+	CalcService calcService;
+	
+	@Test
+	 void add() {
+		
+		assertEquals(100, calcService.add(90, 10));
+	}
+	@Test
+	 void subtract() {
+		
+		assertEquals(10, calcService.subtract(100, 90));
+	}
+	@Test
+	 void divide() {
+		
+		assertEquals(2, calcService.division(2000, 1000));
+	}
+	@Test
+	 void multiply() {
+		
+		assertEquals(90, calcService.multiplication(9, 10));
+	}
+	
+}
